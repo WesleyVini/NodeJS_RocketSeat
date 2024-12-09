145dc54ccff8d5dae750ac0ab19ea6d1a6a0759c
Wesley Vinicius Carvalho da Silva <wesley.vinicius@totvs.com.br>
2024-12-09 17:22:39 -0300

    cria dbfile

--- 01-fundamentos-nodejs/src/database.js
+++ 01-fundamentos-nodejs/src/database.js
@@ -1,12 +1,29 @@
+import fs from 'node:fs/promises'
+
+const dataBasePath = new URL('../db.json', import.meta.url)
 export class Database{
     #database = {}
-
+    
+    constructor(){
+        fs.readFile(dataBasePath, 'utf-8')
+            .then(data =>{
+                this.#database = JSON.parse(data)
+            })
+            .catch(()=>{
+                this.#persist()
+            })
+    }
+    
+    #persist(){
+        fs.writeFile(dataBasePath, JSON.stringify(this.#database))
+    }
+    
     select(table){
         
         const data = this.#database[table] ?? []
         return data
     }
-
+    
     insert(table, data){
         if (Array.isArray(this.#database[table])){
             this.#database[table].push[data]
@@ -14,6 +31,7 @@ export class Database{
             this.#database[table] = [data]
         }
         
+        this.#persist();
         return data;
     }
 }
